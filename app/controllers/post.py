c8e123777bc45ef22d79eb2d3376156f37ed80b2
Svatosha <privitvlad00@gmail.com>
2025-01-12 11:57:26 +0200

    Simpyt fix

--- app/controllers/post.py
+++ app/controllers/post.py
@@ -1,10 +1,12 @@
 from app.models import Post
 
 class PostController:
+    @staticmethod
     def get_post_by_user(user_id: int):
         posts = Post.query.filter_by(user_id=user_id).all()
         return posts
     
+    @staticmethod
     def create_post(title: str, content: str, user_id: int, image: str = None):
         post = Post(
             title=title,
--- app/views/main.py
+++ app/views/main.py
@@ -11,9 +11,13 @@ def index():
 @main_bp.route("/contacts")
 def contacts():
     return render_template(
-        "index.html", title="Контакти", current_page=request.endpoint
+        "contacts.html", title="Контакти", current_page=request.endpoint
     )
 
 @main_bp.route("/read-more")
 def read_more():
     return render_template("focus.html")
+
+@main_bp.route("/popularity")
+def popularity():
+    return render_template("meme/popularity.html")
